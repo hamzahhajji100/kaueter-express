45878a6207e7c076db670e69a390a62134c92a18
hamzahhajji100 <hamzahhajji100@gmail.com>
2026-04-09 14:45:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: "/kaueter-express/",
+    base: "/kraeuter-express/",
   server: {
     host: "::",
     port: 8080,
